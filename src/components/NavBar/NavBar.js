39b4a3ddcacd5cce335bddab2b3891ab9f159321
nitishthorat <nitishthorat112@gmail.com>
2024-07-15 00:32:26 -0700

    Added social handle links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -48,9 +48,9 @@ const NavBar = () => {
               </Nav>
               <span className='navbar-text'>
                 <div className='social-icon'>
-                    <a href="#"><img src={navIcon1} alt=""/></a>
-                    <a href="#"><img src={navIcon2} alt=""/></a>
-                    <a href="#"><img src={navIcon3} alt=""/></a>
+                    <a href="https://www.linkedin.com/in/nitish-uttamrao-thorat/" target='_blank'><img src={navIcon1} alt=""/></a>
+                    <a href="https://github.com/nitishthorat" target='_blank'><img src={navIcon2} alt=""/></a>
+                    <a href="https://www.instagram.com/nitishthorat"><img src={navIcon3} alt=""/></a>
                 </div>
                 <button className='vvd' onClick={() => console.log('connect')}><span>Let's connect</span></button>
               </span>
